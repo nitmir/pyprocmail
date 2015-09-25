b28459f5592c2484ed8eb56c3ef2ab92c3a0f163
Valentin Samir <valentin.samir@crans.org>
2015-09-25 18:34:10 +0200

    add licence and copyright notice

--- parser.py
+++ parser.py
@@ -1,4 +1,14 @@
-# -*- coding: utf-8 -*-
+# ⁻*- coding: utf-8 -*-
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License version 3 for
+# more details.
+#
+# You should have received a copy of the GNU General Public License version 3
+# along with this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# (c) 2015 Valentin Samir
 from pyparsing import *
 unicodePrintables = u''.join(unichr(c) for c in xrange(65536) if not unichr(c).isspace())
 unicodePrintablesSpaces = unicodePrintables + ' \t'
